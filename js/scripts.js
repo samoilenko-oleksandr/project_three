8b67d94d2bd37fe088331abc744f4c0a78b35a76
Oleksandr <klaper1993@gmail.com>
2023-02-15 12:37:11 +0200

    added burger and mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 //  Carousel
 
 $(document).ready(function () {
-    $(".team-items__corousel").slick({
-      dots: true,
-      arrows: false,
+  $(".team-items__corousel").slick({
+    dots: true,
+    arrows: false,
+  });
+  // BURGER
+  $(document).ready(function () {
+    $(".header-burger").click(function (event) {
+      $(".header-burger,.header-menu").toggleClass("active");
+      $("body").toggleClass("lock");
     });
   });
-  
+});
